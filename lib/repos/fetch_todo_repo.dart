8751d35fe24d1295c82f2fd39a445ce33b1db1d4
Aswin Dev <aswin8427@gmail.com>
2024-08-13 01:11:12 +0530

    limit no:s of todo

--- lib/repos/fetch_todo_repo.dart
+++ lib/repos/fetch_todo_repo.dart
@@ -10,7 +10,9 @@ class TodoRepo {
       final response = await http.get(Uri.parse(baseUrl));
       if (response.statusCode == 200) {
         final List<dynamic> jsonData = jsonDecode(response.body);
-        return jsonData.map((item) => TodoModel.fromJson(item)).toList();
+        List<TodoModel> todo =
+            jsonData.map((item) => TodoModel.fromJson(item)).toList();
+        return todo.take(15).toList();
       } else {
         throw Exception('Failed to load data: ${response.statusCode}');
       }
